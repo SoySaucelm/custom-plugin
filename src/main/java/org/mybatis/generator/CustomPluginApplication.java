966003def045607e27fac308dfa9000945fd7f7c
SoySaucelm <soysauce_lm@163.com>
2019-05-15 16:11:29 +0800

    Initial commit

---
+++ src/main/java/org/mybatis/generator/CustomPluginApplication.java
@@ -0,0 +1,13 @@
+package org.mybatis.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomPluginApplication.class, args);
+    }
+
+}
